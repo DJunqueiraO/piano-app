1aa9f76b1ad514695f4c0de0784765c7dfa1bac1
FOURSYS\djunqueira <Batata>
2024-07-30 09:16:30 -0300

    refctor index

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import webview
+import os
 from bs4 import BeautifulSoup
 
-with open('piano-app/build/index.html', 'r') as file:
-    title = (
-        BeautifulSoup(file.read(), 'html.parser')
-        .find('title').text
-    )
 
-webview.create_window(
-    title=title,
-    url='piano-app/build/index.html'
-)
-webview.start(
-    private_mode=False
-)
+class Index:
+
+    def __init__(self, url: str):
+        self.beautifulsoup = None
+        self.url = url
+
+    def get_beautifulsoup(self) -> BeautifulSoup:
+        if self.beautifulsoup is None:
+            with open(self.url, 'r') as file:
+                html = file.read()
+                self.beautifulsoup = BeautifulSoup(html, 'html.parser')
+        return self.beautifulsoup
+
+    def get_title(self) -> str:
+        return self.get_beautifulsoup().find('title').text
+
+
+if __name__ == '__main__':
+
+    url = 'piano-app/build/index.html'
+
+    index = Index(url)
+
+    webview.create_window(
+        title=index.get_title(),
+        url=url
+    )
+    webview.start(
+        private_mode=False
+    )
 
