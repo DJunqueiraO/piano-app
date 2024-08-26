e27e690471b191a4b635df81c49de050e2f2d30e
DJunqueiraO <danielpotoco@gmail.com>
2024-08-26 17:52:49 -0300

    ajustes contador

--- deploy/src/pages/main/midi_player/current_note_input/CurrentNoteInput.tsx
+++ deploy/src/pages/main/midi_player/current_note_input/CurrentNoteInput.tsx
@@ -2,17 +2,23 @@ import { useEffect } from 'react'
 import { Input, InputProps } from '../../../../components/Components'
 import { PianoAudioContext } from '../../../../utils/piano_audio_context/PianoAudioContext'
 import './CurrentNoteInput.css'
+import { useStateAsObject } from '../../../../utils/Utils'
 
 type CurrentNoteInputProps = InputProps & {
 
 }
 
 export function CurrentNoteInput(props: CurrentNoteInputProps) {
-
+  const value = useStateAsObject(PianoAudioContext.current_note)
   return (
     <Input
       {...props}
+      type='number'
       value={PianoAudioContext.current_note}
+      onChange={event => {
+        PianoAudioContext.set_current_note(parseInt(event.target.value))
+        value.set(parseInt(event.target.value))
+      }}
       className={`CurrentNoteInput ${props.className || ''}`}/>
   )
 }
