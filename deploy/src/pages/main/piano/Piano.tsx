e97796a19c2d1e9e0ca104453f8da1c3b32f070e
FOURSYS\djunqueira <Batata>
2024-08-30 10:37:41 -0300

    ajustes visibility change

--- deploy/src/pages/main/piano/Piano.tsx
+++ deploy/src/pages/main/piano/Piano.tsx
@@ -39,21 +39,6 @@ export function Piano(props: PianoProps) {
         }
     }
 
-    useEffect(
-        () => {
-            const handleVisibilityChange = () => {
-                if (document.visibilityState === 'visible') {
-                    refresh()
-                }
-            }
-            document.addEventListener('visibilitychange', handleVisibilityChange)
-            return () => {
-                document.removeEventListener('visibilitychange', handleVisibilityChange)
-            }
-        }, 
-        []
-    )
-
     useEffect(refresh)
 
     const key_button_on_key_down = function(event: React.KeyboardEvent) {
