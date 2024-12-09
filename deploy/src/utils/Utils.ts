4938ecc6a8e99a553ec811f3cca30bce2a6534e3
DJunqueiraO <danielpotoco@gmail.com>
2024-12-09 18:02:46 -0300

    implementação novas features

--- deploy/src/utils/Utils.ts
+++ deploy/src/utils/Utils.ts
@@ -1,3 +1,5 @@
+export { download } from "./download/download"
+
 export { AnimateKeyButton } from "./animate_key_button/AnimateKeyButton"
 
 export { ClearButtons } from "./clear_buttons/ClearButtons"
@@ -7,3 +9,4 @@ export { LocalStorage } from "./local_storage/LocalStorage"
 export { useStateAsObject, UseStateObject } from "./use_state_as_object/useStateAsObject"
 
 export { midiToJson } from "./midi_to_json/midiToJson"
+
