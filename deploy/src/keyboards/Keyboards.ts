70937e6a4fb6fa76742596d207fee619a0de46ff
moran <danielpotoco@gmail.com>
2025-02-25 10:30:30 -0300

    ajustes npc

--- deploy/src/keyboards/Keyboards.ts
+++ deploy/src/keyboards/Keyboards.ts
@@ -28,7 +28,7 @@ export abstract class Keyboards {
       case Keyboards.default_vertical:
         return DefaultVertical
       case Keyboards.default_horizontal_cromatic:
-        return new DefaultHorizontalInterval({division: 6, step: -19})
+        return new DefaultHorizontalInterval({division: 6, step: -19, tuning: {1: 25}})
       case Keyboards.default_horizontal_interval_7:
         return new DefaultHorizontalInterval({tuning: {1: 37, 2: 28, 3: 21, 4: 14, 5: 6}})
       case Keyboards.default_horizontal_interval_5:
